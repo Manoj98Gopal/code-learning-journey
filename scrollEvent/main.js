8f4860cb71c27aa1385b134b07de43b91420be2f
manoj98gopal <manoj98gopal@gmail.com>
2024-07-01 09:03:52 +0530

    minor changes

--- scrollEvent/main.js
+++ scrollEvent/main.js
@@ -4,8 +4,8 @@ const container = document.querySelector(".container");
 button.disabled = true;
 
 const handleScroll = (e) => {
-  console.log(e.currentTarget.scrollTop);
-  console.log(e.currentTarget.scrollHeight);
+  // console.log(e.currentTarget.scrollTop);
+  // console.log(e.currentTarget.scrollHeight);
 };
 
 const handleObserve = (payload) => {
@@ -28,3 +28,16 @@ const watching = new IntersectionObserver(handleObserve, {
 watching.observe(container.lastElementChild);
 
 container.addEventListener("scroll", handleScroll);
+
+// const call = async () => {
+//   console.log("starting");
+//   const data = await new Promise((resove) => {
+//     setTimeout(() => {
+//       resove(5);x 
+//       console.log("calling the set");
+//     }, 5000);
+//   });
+//   console.log("ending");
+// };
+
+// call();
