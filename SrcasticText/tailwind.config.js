37b8bbe0fe90606f1be4d64edd1862cae223d715
manoj98gopal <manoj98gopal@gmail.com>
2024-09-15 09:56:29 +0530

    srcastic text project created  with percel and tailwind css

---
+++ SrcasticText/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './*.html', // Include any HTML files in the root directory
+    './src/**/*.js', // Include any JS files in the 'src' directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
